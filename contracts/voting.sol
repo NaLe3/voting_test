f46a6ce1ae0b7c7828fb1fc62fdf57208f2046cc
Nathan Lebas <nathanlebas3@gmail.com>
2022-03-17 11:22:54 -0300

    create function to manage state by Owner only

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -6,12 +6,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Voting {
 
-  uint winningProposalId; 
+  uint winningProposalId;
+  WorkflowStatus public workfowState = WorkflowStatus.RegisteringVoters;
 
   struct Voter {
     bool isRegistered;
     bool hasVoted;
     uint votedProposalId;
+    uint winningProposalId;
   }
 
   struct Proposal {
@@ -32,6 +34,17 @@ contract Voting {
   event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
   event ProposalRegistered(uint proposalId);
   event Voted (address voter, uint proposalId);
+
+
+  // worflow status manageble by Owner only
+  function turnStateToRegisteringVoters() public onlyOwner { workfowState = WorkflowStatus.RegisteringVoters; }
+  function turnStateToProposalsRegistrationStarted() public onlyOwner { workfowState = WorkflowStatus.ProposalsRegistrationStarted; }
+  function turnStateToProposalsRegistrationEnded() public onlyOwner { workfowState = WorkflowStatus.ProposalsRegistrationEnded; }
+  function turnStateToVotingSessionStarted() public onlyOwner { workfowState = WorkflowStatus.VotingSessionStarted; }
+  function turnStateToVotingSessionEnded() public onlyOwner { workfowState = WorkflowStatus.VotingSessionEnded; }
+  function turnStateToVotesTallied() public onlyOwner { workfowState = WorkflowStatus.VotesTallied; }
+
+
 
 
 } 
