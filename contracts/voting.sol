4a96505c08a1120bf0a14fec55a212ac91824312
Nathan Lebas <nathanlebas3@gmail.com>
2022-03-21 15:02:21 -0300

    update comments

--- contracts/voting.sol
+++ contracts/voting.sol
@@ -36,18 +36,24 @@ contract Voting is Ownable{
   event ProposalRegistered(uint proposalId);
   event Voted (address voter, uint proposalId);
 
-  // Adding the Owner as a potential voter
+  // Automatically adding the contract owner as a potential voter
   constructor() {
     votersWhiteList[msg.sender] = Voter(true, false, 0);
     workflowState = WorkflowStatus.RegisteringVoters;
   }
 
+  // Ensure the address is listed in the whitelist to participate in the voting
   modifier isWhiteListed(address _address) {
      require(votersWhiteList[_address].isRegistered == true, "Not regesitered");
     _;
   }
 
-  modifier proposerIdAllowed(uint _id) {
+  /*
+    Ensure the id proposal is correct
+    For the Voter the proposal ID starts at "1" up to to the number of proposal (proposals.length)
+
+  */
+  modifier proposalIdAllowed(uint _id) {
     require(
       0 < _id && _id <= proposals.length,
       "This ID does not exist" 
@@ -80,14 +86,14 @@ contract Voting is Ownable{
     } else if (currentState  == WorkflowStatus.VotingSessionStarted) {
       workflowState = currentState =  WorkflowStatus.VotingSessionEnded;
       previousState = WorkflowStatus.VotingSessionStarted;
-      CurrentStateToReturn = "Voting session has ended, the Administrator is proceeding to the vote tally"; 
+      CurrentStateToReturn = "Voting session has ended, the Administrator needs to proceed to the vote tally"; 
     } 
 
     emit WorkflowStatusChange(previousState, currentState);
     return CurrentStateToReturn;
   }
    
-  //The owner only can register voter 
+  //The owner only can register new voter 
   function registringVoter(address _address) public onlyOwner {
     require(workflowState == WorkflowStatus.RegisteringVoters, "Voter register is closed");
     require(votersWhiteList[_address].isRegistered == false, "Already registered");
@@ -95,8 +101,10 @@ contract Voting is Ownable{
     emit VoterRegistered(_address);
   } 
 
-  // Registring voter poposal
-  //@params the indexes of the array proposal are use as the proposal _id 
+  /*
+  Registring voter poposal
+  @params the indexes of the array proposal are use as the proposal _id 
+  */
   function regsitringProposal(string memory _proposal) public isWhiteListed(msg.sender){
     require(workflowState == WorkflowStatus.ProposalsRegistrationStarted, "Proposal register is closed");
     proposals.push(Proposal(_proposal, 0));
@@ -104,18 +112,19 @@ contract Voting is Ownable{
   }
 
   /* 
-  Get proposal with proposals array index 
-  @params _id start at "1" for the voter
-  therefore as we use the proposals[] index for proposalId
-  we need to do proposals[_id - 1] to return the proposal decription expected
+  @params _id is the proposal ID. It starts at "1" for the voter,
+  so we need to do proposals[_id - 1] to retreive the corresponding index
   */
-  function getProposal(uint _id) public view isWhiteListed(msg.sender) proposerIdAllowed(_id) returns(string memory){
+  function getProposal(uint _id) public view isWhiteListed(msg.sender) proposalIdAllowed(_id) returns(string memory){
     require(proposals.length != 0, "No proposals yet");
     return proposals[_id - 1].description;
   }
 
-  //@params _id start at "1" for the voter
-  function vote(uint _id) public isWhiteListed(msg.sender) proposerIdAllowed(_id) {
+  /*
+  @params _id is the proposal ID. It starts at "1" for the voter, 
+  so we need to do proposals[_id - 1] to retreive the corresponding index
+  */
+  function vote(uint _id) public isWhiteListed(msg.sender) proposalIdAllowed(_id) {
     require(workflowState == WorkflowStatus.VotingSessionStarted, "Vote session is closed");
     require(votersWhiteList[msg.sender].hasVoted == false, "Already voted");
     proposals[_id - 1].voteCount ++;
@@ -130,7 +139,10 @@ contract Voting is Ownable{
     return proposals[proposalId].description;
   }
 
-  // We look for the largest voteCount and it index (as we use the index as proposal Id)
+  /* We look for the largest voteCount by iterating in each proposals elements, 
+  anytime the value(uint voteCount) is higher than the largestCournt, we replace 
+  the later by this value.  
+  */
   function voteTally() public onlyOwner {
     require(workflowState == WorkflowStatus.VotingSessionEnded, "Vote session must be closed");
     uint256 largestCount;
@@ -152,11 +164,9 @@ contract Voting is Ownable{
     
   }
 
+  // Retreive the winning proposal description 
   function getWinner() public view isWhiteListed(msg.sender) returns(string memory) {
-    require(
-      workflowState == WorkflowStatus.VotesTallied, 
-      "Result is not published yet"
-    );
+    require(workflowState == WorkflowStatus.VotesTallied, "Result is not published yet");
     return proposals[winningProposalId].description;
   }
 
